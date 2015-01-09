4bb72fea4bd2bfaa6ef54843c8a90cd7bc1178bc
sportnak <david.m.nakayama@vanderbilt.edu>
2015-01-08 22:22:14 -0600

    added a database and upgrade/downgrade/migrate/create scripts

---
+++ db_repository/manage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from migrate.versioning.shell import main
+
+if __name__ == '__main__':
+    main(six='<module 'six' from 'C:\\Users\\Michael\\Documents\\Development\\flask-microblog\\flask\\lib\\site-packages\\six.py'>')
