f1d16132904683ac2545197721d38fca3dfe0120
sportnak <david.m.nakayama@vanderbilt.edu>
2015-01-09 10:57:15 -0600

    remove mail

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 WTF_CSRF_ENABLED = True
@@ -17,12 +17,6 @@ OPENID_PROVIDERS = [
 	{'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
 	{'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
 	{'name': 'MyOpenId', 'url': 'https://www.myopenid.com'}]
-
-#mail server settings
-MAIL_SERVER = 'localhost'
-MAIL_PORT = 25
-MAIL_USERNAME = None
-MAIL_PASSWORD = None
 
 #administrator list
 ADMINS = ['sportnak@gmail.com']
