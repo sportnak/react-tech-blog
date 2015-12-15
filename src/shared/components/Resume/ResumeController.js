502c097ed1abc0d0959e69a5d155dcb887bf9bfe
Michael Nakayama <michael.nakayama@WS2206-4.attlocal.net>
2015-12-14 23:59:38 -0600

    Resume update.

--- src/shared/components/Resume/ResumeController.js
+++ src/shared/components/Resume/ResumeController.js
@@ -26,23 +26,85 @@ var ResumeController = React.createClass({
       document.getElementsByClassName('circle__main')[0].style.transform = ("rotate(-120deg)");
     }
   },
+  renderSkills() {
+    return (  
+      <div className="right__skills">
+        <ul>
+          <li className="skill">
+            <div className="skill__container">
+              <span>Javascript<span className="optional">(React, Node, jQuery, etc.)</span> :</span>
+              <div className="skill__container__language javascript">
+                <div className="javascript"/>
+                <div className="cover">Majority Focus</div>
+              </div>
+            </div>
+          </li>
+          <li className="skill">
+            <div className="skill__container">
+              <span>C#<span className="optional">(Razor, ASP.net, etc.)</span> :</span>
+              <div className="skill__container__language">
+                <div className="c-sharp"/>
+                <div className="cover">Current Backend</div>
+              </div>
+            </div>
+          </li>
+          <li className="skill">
+            <div className="skill__container">
+              <span>HTML & CSS:</span>
+              <div className="skill__container__language">
+                <div className="html-css"/>
+                <div className="cover">Its kinda basic</div>
+              </div>
+            </div>
+          </li>
+          <li className="skill">
+            <div className="skill__container">
+              <span>Java:</span>
+              <div className="skill__container__language">
+                <div className="java"/>
+                <div className="cover">TA and Android Projects</div>
+              </div>
+            </div>
+          </li>
+          <li className="skill">
+            <div className="skill__container">
+              <span>Python<span className="optional">(Flask)</span> :</span>
+              <div className="skill__container__language">
+                <div className="python"/>
+                <div className="cover">Couple Projects</div>
+              </div>
+            </div>
+          </li>
+          <li className="skill">
+            <div className="skill__container">
+              <span>C++:</span>
+              <div className="skill__container__language">
+                <div className="c-plus-plus"/>
+                <div className="cover">TA for ADSt class</div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  },
   renderVanderbilt() {
     return [
-      <h2 classname="main__work__info__company"><a href="https://vanderbilt.edu">{this.state.companyName}</a></h2>,
+      <h2 className="main__work__info__company"><a href="https://vanderbilt.edu">{this.state.companyName}</a></h2>,
       <p>{'Vanderbilt University is my alma mater. Located in Nashville, Tennessee, the school is a top 20 University that dedicates itself to excellence.'}</p>,
       <p>{'Here I worked as a Teaching Assistant where I held office hours and graded for the first semester and second semester classes. I answered questions on Java and C++ as well as general programming questions.'}</p>
     ];
   },
   renderGivetoken() {
     return [
-      <h2 classname="main__work__info__company"><a href="https://givetoken.com">{this.state.companyName}</a></h2>,
+      <h2 className="main__work__info__company"><a href="https://givetoken.com">{this.state.companyName}</a></h2>,
       <p>{'Give Token is a startup in Nashville that currently focuses on making the recruitment process more engaging and meaningful.'}</p>,
       <p>{'At Give Token, I worked with connecting the token creation with many social media sites and designing the pages in Javascript, CSS, HTML, and PHP.'}</p>
     ];
   },
   renderFaithlife() {
     return [
-      <h2 classname="main__work__info__company"><a href="https://www.faithlife.com">{this.state.companyName}</a> - Current</h2>,
+      <h2 className="main__work__info__company"><a href="https://www.faithlife.com">{this.state.companyName}</a> - Current</h2>,
       <p>{'Faithlife Corporation is a Bible software company based out of  Bellingham, Washington. We provide a wide variety of products that allows people to do more and better Bible study'}</p>,
       <p>{'I work specifically with the Faithlife platform team, spending time doing alot of Javascript, C#, HTML, and CSS.'}</p>
     ];
@@ -84,12 +146,38 @@ var ResumeController = React.createClass({
               </div>
             </div>
             <div className="main__projects">
+              <h2 className="projects__title">Personal Projects</h2>
             </div>
             <div className="main__education">
+              <h2 className="main__education__title">Education</h2>
+              <ul>
+                <li className="school__container">
+                  <div className="school-container">
+                    <div className="school">
+                      <img className="school__image" src="/vanderbilt_logo.png" /> 
+                    </div>
+                  </div>
+                  <div className="description">
+                    <h2 className="description__name">Vanderbilt University</h2>
+                    <h3 className="level">Bachelor of Computer Science</h3>
+                    <h4>Relevant Classes:</h4>
+                    <p>{'Intro to Java, Intro to C++, Intermediate C++ with Patterns, Discrete Structures, Computer Organization,Algorithms, Operating Systems I & II,Study of Programming Languages,Special Topics: Unmanned Aircrafts, Computer Networks, Computer Ethics, Artificial Intelligence, Network Security, Error Correcting Codes and Cryptography, and Principles of Software Engineering'}</p>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="right">
-            <div className="right__skills">
+            {this.renderSkills()}
+            <div className="right__other">
+              <p className="right__other__title">Other Skills</p>
+              <ul>
+                <li>Photography: Nikon D3200</li>
+                <li>Design: this website</li>
+                <li>Automotive: Wrangler owner</li>
+                <li>Woodworking: kinda</li>
+                <li>Crackin jokes in serious places</li>
+              </ul>
             </div>
           </div>
         </div>
